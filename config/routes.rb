b9b48794daeb272b77b435ddd33c87f684204ce8
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2009-01-05 13:36:33 +0000

    Revert "basic User model for #1"
    This reverts commit 21408e059ac0591f00ac46f5a4dbd75010fa57ac.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :users
-
   map.resources :providers
 
   # The priority is based upon order of creation: first created -> highest priority.
