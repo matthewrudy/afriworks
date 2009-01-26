81a2a9e22adae6de5f88e252b56f5d1942bc537f
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2009-01-26 20:33:29 +0000

    include and style proposals based on their status (re: #13)

--- db/migrate/20090126170827_add_status_to_contracts.rb
+++ db/migrate/20090126170827_add_status_to_contracts.rb
@@ -1,6 +1,6 @@
 class AddStatusToContracts < ActiveRecord::Migration
   def self.up
-    add_column :contracts, :status, :string
+    add_column :contracts, :status, :string, :null => false, :default => "draft"
     add_index  :contracts, :status # makes sense to have an index
   end
 
